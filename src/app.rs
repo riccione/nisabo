129ad07cc7000dffbaafa942925cca623618555e
Zero <zero@zero-ms7c56.ignes-zero.com>
2025-05-08 23:02:07 -0400

    Add styles for main screen

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,17 @@
+use std::path::{PathBuf};
+
 #[derive(Default)]
-pub struct App {}
+pub struct App {
+    pub archive_name: String,
+    archive_path: Option<PathBuf>
+}
 
 impl App {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let mut app = Self {
+            archive_name: String::new(),
+            archive_path: None,
+        };
         // customize egui with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals
         Self::default()
     }
--- src/app_eframe.rs
+++ src/app_eframe.rs
@@ -1,11 +1,35 @@
-use eframe::egui;
+use eframe::egui::{self, RichText, Color32, Button};
 use crate::app::App;
 use crate::APP_NAME;
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(APP_NAME);
+            ui.vertical_centered(|ui| {
+                ui.add_space(20.0);
+
+                ui.label("Archive Name [optional]: ");
+                ui.text_edit_singleline(&mut self.archive_name);
+                
+                ui.add_space(20.0);
+
+                let softer_red = Color32::from_rgb(200, 50, 50);
+                let txt_create_archive = RichText::new("Create Archive")
+                    .size(24.0)
+                    .color(Color32::WHITE);
+                let btn_create_archive = Button::new(txt_create_archive)
+                    .fill(softer_red);
+
+                if ui.add(btn_create_archive).clicked() {
+                    println!("Create Archive clicked");
+                }
+                
+                ui.add_space(20.0);
+
+                if ui.button("Open Archive").clicked() {
+                    println!("Open Archive clicked");
+                }
+            });
         });
     }
 }
