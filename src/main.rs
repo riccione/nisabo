af11be2e9424871c71d518cc8eda1e3e04449a4c
Zero <zero@zero-ms7c56.ignes-zero.com>
2025-05-08 21:33:41 -0400

    Create a stub for the app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+
+fn main() -> Result<(), eframe::Error> {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        APP_NAME,
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc))))
+    )
+}
+
+#[derive(Default)]
+struct App {}
+
+impl App {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // customize egui with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals
+        Self::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading(APP_NAME);
+        });
+    }
 }
