95e8cab4f433414be4a371b60468df768cf25def
riccione <riccione@protonmail.com>
2025-07-01 22:19:18 -0400

    Remove commented code

--- src/ui/sidepanels.rs
+++ src/ui/sidepanels.rs
@@ -36,33 +36,6 @@ impl App {
                 }
             });
         
-        /*
-        if self.state_is_right_panel_on {
-            egui::SidePanel::right("right panel")
-                .resizable(true)
-                .default_width(200.0)
-                .show(ctx, |ui| {
-                    render_md(ui, &self.edited_content);
-                });
-        }
-
-        egui::CentralPanel::default()
-            .show(ctx, |ui| {
-                egui::ScrollArea::vertical()
-                    //.auto_shrink([false; 2])
-                    .auto_shrink([true; 2])
-                    .show(ui, |ui| {
-                        if let Some(_) = self.selected_index {
-                            ui.add(
-                                egui::TextEdit::multiline(&mut self.edited_content)
-                                    .lock_focus(true)
-                                    .desired_width(f32::INFINITY)
-                            );
-                        }
-                    });
-        });
-        */
-
         egui::CentralPanel::default()
             .show(ctx, |ui| {
                 egui::ScrollArea::vertical()
